54d1aecb9cabc86c8efbf1e82a3cde75cd84c9d0
Ryan Pfeiffer <rjpfeiffer28@outlook.com>
2024-11-23 21:57:24 -0500

    Created database for dad jokes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 import pyjokes
 import random
 import requests
@@ -6,18 +7,42 @@ from dadjokes import Dadjoke
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dadjokes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 jokes = pyjokes.get_jokes("en")
 programmingJokeList = []
 dadJokeList = []
 for i in jokes:
         programmingJokeList.append(i)
 
+class DadJoke(db.Model):
+      id = db.Column(db.Integer, primary_key=True)
+      joke = db.Column(db.String(500), nullable=False)
+      
+      def __repr__(self):
+            return f'<DadJoke {self.id}: {self.joke}>'
+def create_db():
+      with app.app_context():
+            db.create_all()
 def CollectDadJokes():
-      i = 0
-      while i < 100:
-            dadJoke = Dadjoke().joke
-            dadJokeList.append(dadJoke)
-            i += 1
+    with app.app_context():
+        # Correct query to count the number of jokes in the database
+        if db.session.query(DadJoke).count() == 0:  # Corrected this line
+            print("No jokes found in the database. Adding jokes now...")
+            i = 0
+            while i < 100:
+                dadJoke = Dadjoke().joke
+                new_joke = DadJoke(joke=dadJoke)
+                db.session.add(new_joke)  # Add the new joke
+                if i % 10 == 0:  # Commit every 10 jokes for better performance
+                    db.session.commit()
+                i += 1
+            db.session.commit()  # Commit any remaining jokes
+        else:
+            print("Jokes already exist in the database.")
+
+create_db
 CollectDadJokes()
 
 @app.route("/")
@@ -31,5 +56,7 @@ def get_programming_joke():
 
 @app.route("/newdadjoke")
 def get_dad_joke():
-    dadJoke = random.choice(dadJokeList)
-    return render_template("index.html", dadJoke=dadJoke)
+    random_dad_joke = DadJoke.query.order_by(db.func.random()).first()
+    return render_template("index.html", dadJoke=random_dad_joke.joke)
+if __name__ == "__main__":
+      app.run(debug=True)
